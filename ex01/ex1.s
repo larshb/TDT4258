841a4a7f5c93305d3e3abd25dee86dc6b1ba887c
Lars Henrik Bolstad <lahebo@gmail.com>
2016-09-05 17:43:38 +0200

    working LEDs on button press

--- ex01/ex1.s
+++ ex01/ex1.s
@@ -81,8 +81,45 @@
 	.globl  _reset
 	.type   _reset, %function
 	.thumb_func
-_reset: 
-	b .  // do nothing
+_reset:
+
+	// Initialize I/O registers
+	ldr r6, =GPIO_PA_BASE
+	ldr r7, =GPIO_PC_BASE
+
+	// Enabling GPIO-clock
+	ldr r1, =CMU_BASE
+	ldr r2, [r1, #CMU_HFPERCLKEN0]
+	mov r3, #1
+	lsl r3, r3, #CMU_HFPERCLKEN0_GPIO
+	orr r2, r2, r3
+	str r2, [r1, #CMU_HFPERCLKEN0]
+
+	// Setting drive strength on output pins
+	mov r2, #0x2
+	str r2, [r6, #GPIO_CTRL]
+
+	// Set LEDs to output (pin 8-15)
+	mov r2, #0x55555555
+	str r2, [r6, #GPIO_MODEH]
+
+	// Initialize LEDs off
+	mov r2, #0xff00
+	str r2, [r6, #GPIO_DOUT]
+
+	// Setting buttons to input (pin 0-7)
+	mov r2, #0x33333333
+	str r2, [r7, #GPIO_MODEL]
+
+	// Enable button pullup
+	mov r2, #0xff
+	str r2, [r7, #GPIO_DOUT]
+
+mainloop:
+	ldr r2, [r7, #GPIO_DIN]
+	lsl r2, r2, #8
+	str r2, [r6, #GPIO_DOUT]
+	b mainloop
 
 /////////////////////////////////////////////////////////////////////////////
 //
@@ -92,12 +129,11 @@ _reset:
 /////////////////////////////////////////////////////////////////////////////
 
     .thumb_func
-gpio_handler:  
+gpio_handler:
 	b .  // do nothing
 
 /////////////////////////////////////////////////////////////////////////////
 
     .thumb_func
-dummy_handler:  
+dummy_handler:
 	b .  // do nothing
-
