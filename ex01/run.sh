dbf955dc38c4cba60f152714d0577813658fc81f
Lars Henrik Bolstad <lahebo@gmail.com>
2018-09-13 14:02:19 +0200

    make run script use make upload

--- ex01/run.sh
+++ ex01/run.sh
@@ -1,1 +1,11 @@
-make & eACommander.sh -r --address 0x00000000 -f "ex1.bin" -r
+#!/bin/bash
+
+make #2> /dev/null
+
+if [ $? -eq 0 ]
+then
+  echo "Successfully compiled, flashing binary..."
+  make upload #eACommander.sh -r --address 0x00000000 -f "ex1.bin" -r
+else
+  echo "Compile error!" >&2
+fi
