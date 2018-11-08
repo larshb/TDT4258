34ac6d787faa0b55e7808d74c0b684d9f02ca723
Lars Henrik Bolstad <lahebo@gmail.com>
2018-11-08 02:23:54 +0100

    Even more graphics\!

--- ex03/local_src/game-1.0/screen.c
+++ ex03/local_src/game-1.0/screen.c
@@ -17,11 +17,18 @@ int screen_fd;
 struct fb_copyarea screen_ca = {0, 0, SCREEN_WIDTH, SCREEN_HEIGHT};
 uint16_t* screen_ar;
 
-uint16_t   RED = 0b1111100000000000;
-uint16_t WHITE = 0b1111111111111111;
-uint16_t  BLUE = 0b0000000000011111;
-uint16_t  SKIN = 0b1111111001010110;
-uint16_t BLACK = 0b0000000000000000;
+#define  WHITE 0b1111111111111111
+#define  BLACK 0b0000000000000000
+
+#define    RED 0b1111100000000000
+#define YELLOW 0b1111111111100000
+#define  GREEN 0b0000011111100000
+#define   CYAN 0b0000011111111111
+#define   BLUE 0b0000000000011111
+#define PURPLE 0b1111100000011111
+
+#define   SKIN 0b1111111001010110
+
 
 #define TRY(exp, str) \
 	if ((exp) < 0) { \
@@ -62,8 +69,26 @@ void draw_block(uint16_t x, uint16_t y, uint16_t dx, uint16_t dy, uint16_t* data
 	}
 }
 
+#define ABS(a) (a<0?-a:a)
+void draw_line(int16_t x0, int16_t y0, int16_t x1, int16_t y1, int16_t color) {
+	int16_t step;
+	int16_t dx = x1-x0;
+	int16_t dy = y1-y0;
+	step = (ABS(dx)>ABS(dy)) ? ABS(dx) : ABS(dy);
+	float dxf = (float)dx/step;
+	float dyf = (float)dy/step;
+	float x = x0;
+	float y = y0;
+	int16_t i = 0;
+	while (i++ < step) {
+		screen_ar[(int)y*SCREEN_WIDTH+(int)x] = color;
+		x = x+dxf;
+		y = y+dyf;
+	}
+}
+
 void screen_clear() {
-	draw_rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
+	draw_rectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, BLACK);
 }
 
 #define BLOCK SCREEN_HEIGHT/16
@@ -90,10 +115,67 @@ void cdraw(uint16_t* out, char* in, uint16_t n) {
 	}
 }
 
-void screen_test() {
-	screen_init();
-	//draw_flag();
-	screen_clear();
+int8_t sine[40] = {0,15,30,45,58,70,80,89,95,98,100,98,95,89,80,70,58,45,30,15,0,-16,-31,-46,-59,-71,-81,-90,-96,-99,-100,-99,-96,-90,-81,-71,-59,-46,-31,-16};
+int8_t cosine[40] = {100,98,95,89,80,70,58,45,30,15,0,-16,-31,-46,-59,-71,-81,-90,-96,-99,-100,-99,-96,-90,-81,-71,-59,-46,-31,-16,-1,15,30,45,58,70,80,89,95,98};
+
+typedef struct {
+	uint16_t x, y;
+} coords_t;
+
+void animate_diamond() {
+	int t;
+	for (;;) {
+		for (t = 0; t < 40; t++) {
+			screen_clear();
+			coords_t a, b, c, d, e, f;
+			a.x = 160-cosine[t]/5;
+			a.y = 10;
+			b.x = 160+cosine[t];
+			b.y = 100;
+			c.x = 160+cosine[t]/5;
+			c.y = 230;
+			d.x = 160-cosine[t];
+			d.y = 140;
+			e.x = 160+sine[t];
+			e.y = 120;
+			f.x = 160-sine[t];
+			f.y = 120;
+			draw_line(a.x, a.y, b.x, b.y, YELLOW);
+			draw_line(b.x, b.y, c.x, c.y, YELLOW);
+			draw_line(a.x, a.y, d.x, d.y, YELLOW);
+			draw_line(d.x, d.y, c.x, c.y, YELLOW);
+			draw_line(a.x, a.y, e.x, e.y, YELLOW);
+			draw_line(e.x, e.y, c.x, c.y, YELLOW);
+			draw_line(a.x, a.y, f.x, f.y, YELLOW);
+			draw_line(f.x, f.y, c.x, c.y, YELLOW);
+			draw_line(b.x, b.y, e.x, e.y, YELLOW);
+			draw_line(e.x, e.y, d.x, d.y, YELLOW);
+			draw_line(d.x, d.y, f.x, f.y, YELLOW);
+			draw_line(f.x, f.y, b.x, b.y, YELLOW);
+			screen_refresh();
+		}
+	}
+}
+
+void draw_gridlines() {
+	int r, c;
+	for (r = 0; r < 320; r+=2) {
+		for (c = 0; c < 240; c+=2) {
+			draw_line(0,c,r,c, RED); //UL
+			draw_line(r,0,r,c, BLUE); //UL
+		}
+		screen_refresh();
+	}
+}
+
+void draw_corners() {
+	draw_line(0,0,10,10,RED); //UL
+	draw_line(309,10,319,0, BLACK); //UR
+	draw_line(0,239,10,229, BLUE); //LL
+	draw_line(319,239,309,229, SKIN); //LR
+}
+
+void draw_mario() {
 	char mario_str[16*16] = {"WWWWWBBBBBWWWWWWWWWWBRRRRRBBBWWWWWWBRRRRRRRRRBWWWWWBBBSSBSBBBWWWWWBSSBBSBSBBBWWWWWBSSBBBSSBBBWWWWWWBBSSSBBBBBWWWWWWBBBSSSSSBWWWWWBBRRRBBRRBBBWWWBSSRRRBBRRBRBWWWBSSSRRBBBBBBRSBWWBSSBBBBSBBSBSBWWWBBBBBBBBBRRBWWWBRRBBBBBBRRRBWWWBRRRBWWBRRRBWWWWWBBBWWWWBBBWWWW"};
 	uint16_t mario[16*16];
 	cdraw(mario, mario_str, 16*16);
@@ -102,6 +184,11 @@ void screen_test() {
 		for (j = 0; j < 240; j+=16) {
 			draw_block(i, j, 16, 16, mario);
 		}
-	}	
+	}
+}
+
+void screen_test() {
+	screen_init();
+	animate_diamond();
 	screen_refresh();
 }
