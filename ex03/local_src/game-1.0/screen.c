ec2b9f93da845108848963b0ea8521db14cd2fa4
Lars Henrik Bolstad <lahebo@gmail.com>
2018-11-18 21:24:14 +0100

    Fix xy swap and add text support

--- ex03/local_src/game-1.0/screen.c
+++ ex03/local_src/game-1.0/screen.c
@@ -49,22 +49,23 @@ inline int screen_refresh_area(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y
 
 void draw_rectangle(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint16_t color) {
 	uint16_t r, c;
-	for (r = x0; r < x1; r++) {
-		for (c = y0; c < y1; c++) {
-			screen_ar[c*SCREEN_WIDTH+r] = color;
+	for (c = x0; c < x1; c++) {
+		for (r = y0; r < y1; r++) {
+			screen_ar[r*SCREEN_WIDTH+c] = color;
 		}
 	}
 	screen_refresh_area(x0, y0, x1-x0, y1-y0);
 }
 
 /* Draw block of data from upper-left [x/y] with dimensions [dx/dy] */
-void draw_block(uint16_t x, uint16_t y, uint16_t dx, uint16_t dy, uint16_t* data) {
+void draw_block(uint16_t* data, uint16_t x, uint16_t y, uint16_t dx, uint16_t dy) {
 	uint16_t r, c;
-	for (r = x; r < x+dx; r++) {
-		for (c = y; c < y+dy; c++) {
-			screen_ar[c*SCREEN_WIDTH+r] = data[c*dx+r];
+	for (c = 0; c < dx; c++) {
+		for (r = 0; r < dy; r++) {
+			screen_ar[(r+y)*SCREEN_WIDTH+(c+x)] = data[r*dx+c];
 		}
 	}
+	screen_refresh_area(x, y, dx, dy);
 }
 
 #define ABS(a) (a<0?-a:a)
@@ -177,16 +178,75 @@ void draw_mario() {
 	char mario_str[16*16] = {"WWWWWBBBBBWWWWWWWWWWBRRRRRBBBWWWWWWBRRRRRRRRRBWWWWWBBBSSBSBBBWWWWWBSSBBSBSBBBWWWWWBSSBBBSSBBBWWWWWWBBSSSBBBBBWWWWWWBBBSSSSSBWWWWWBBRRRBBRRBBBWWWBSSRRRBBRRBRBWWWBSSSRRBBBBBBRSBWWBSSBBBBSBBSBSBWWWBBBBBBBBBRRBWWWBRRBBBBBBRRRBWWWBRRRBWWBRRRBWWWWWBBBWWWWBBBWWWW"};
 	uint16_t mario[16*16];
 	cdraw(mario, mario_str, 16*16);
-	int i, j;
-	for (i = 0; i < 320; i+=16) {
-		for (j = 0; j < 240; j+=16) {
-			draw_block(i, j, 16, 16, mario);
+	draw_block(mario, 160, 120, 16, 16);
+}
+
+uint16_t* font_getsymbol(char c);
+
+/* Print characters based on 8x8 grid */
+void screen_writechar(char c, uint8_t x, uint8_t y) {
+	draw_block(font_getsymbol(c), x*8, y*8, 8, 8);
+}
+
+void screen_print(char* str, uint8_t x, uint8_t y) {
+	char* c = str;
+	do {
+		if (*c == '\n') {
+			y++;
+		}
+		else {
+			screen_writechar(*c, x++, y);
+		}
+	} while (*(++c) != 0);
+}
+
+void ascii_table() {
+	uint8_t r, c;
+	for (r = 0; r <= 8; r++) {
+		for (c = 0; c <= 9; c++) {
+			screen_writechar(r*9+c, r, c);
 		}
 	}
 }
 
+void darth_plagueis() {
+	int r = 1;
+	screen_print("Did you ever hear the tragedy of Darth",0,r++);
+	screen_print("Plagueis the Wise? I thought not. It's",0,r++);
+	screen_print("not a story the Jedi would tell you.  ",0,r++);
+	screen_print("It's a Sith legend. Darth Plagueis was",0,r++);
+	screen_print("a Dark Lord of the Sith, so powerful  ",0,r++);
+	screen_print("and so wise he could use the Force to ",0,r++);
+	screen_print("influence the midichlorians to create ",0,r++);
+	screen_print("life... He had such a knowledge of the",0,r++);
+	screen_print("dark side that he could even keep the ",0,r++);
+	screen_print("ones he cared about from dying. The   ",0,r++);
+	screen_print("dark side of the Force is a pathway to",0,r++);
+	screen_print("many abilities some consider to be    ",0,r++);
+	screen_print("unnatural. He became so powerful...   ",0,r++);
+	screen_print("the only thing he was afraid of was   ",0,r++);
+	screen_print("losing his power, which eventually, of",0,r++);
+	screen_print("course, he did. Unfortunately, he     ",0,r++);
+	screen_print("taught his apprentice everything he   ",0,r++);
+	screen_print("knew, then his apprentice killed him  ",0,r++);
+	screen_print("in his sleep. It's ironic he could    ",0,r++);
+	screen_print("save others from death, but not       ",0,r++);
+	screen_print("himself.                              ",0,r++);
+}
+
+void font_init();
+
 void screen_test() {
 	screen_init();
-	animate_diamond();
+	font_init();
+	//animate_diamond();
+	//ascii_table();
+
+	//draw_mario();
+
+	//screen_print("Hade bra, Didrik!", 5, 5);
+
+	darth_plagueis();
+
 	screen_refresh();
 }
